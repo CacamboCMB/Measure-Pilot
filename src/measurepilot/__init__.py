7f75880ba22c52309fcddb48010b619efc6ec6f9
CacamboCMB <david.stiefel@icloud.com>
2026-08-30 20:29:23 +0200

    MP-0002: export calibrated capture API

--- src/measurepilot/__init__.py
+++ src/measurepilot/__init__.py
@@ -1,14 +1,34 @@
 """MeasurePilot core package."""
 
+from .calibration import (
+    ARUCO_DICTIONARY_NAME,
+    CALIBRATION_LAYOUT_VERSION,
+    DEFAULT_LAYOUT,
+    MARKER_IDS,
+    CalibrationLayout,
+    calibration_pdf_bytes,
+    write_calibration_pdf,
+)
 from .model import INTERNAL_LENGTH_UNIT, SCHEMA_VERSION, MeasurePilotProject
 from .project import load_project, save_project
+from .rectification import RectificationResult, rectify_file, rectify_image
 
 __all__ = [
+    "ARUCO_DICTIONARY_NAME",
+    "CALIBRATION_LAYOUT_VERSION",
+    "DEFAULT_LAYOUT",
     "INTERNAL_LENGTH_UNIT",
+    "MARKER_IDS",
     "SCHEMA_VERSION",
+    "CalibrationLayout",
     "MeasurePilotProject",
+    "RectificationResult",
+    "calibration_pdf_bytes",
     "load_project",
+    "rectify_file",
+    "rectify_image",
     "save_project",
+    "write_calibration_pdf",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
